72da377799393663388d0f3e0502e07899fb0b42
Deepak Dewani <deepak.dewani@accionlabs.com>
2018-10-23 14:41:48 +0530

    ADD(server): mongoDB connection and webpack middleware

--- server/server.js
+++ server/server.js
@@ -1,15 +1,35 @@
-const express = require('express')
-const expressGraphQL = require('express-graphql')
+const express = require('express');
+const models = require('./models');
+const expressGraphQL = require('express-graphql');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const schema = require('./schema/schema');
 
-const app = express()
+const app = express();
 
-const schema = require('./schema/schema')
+const MONGO_URI = 'mongodb://127.0.0.1/testdb';
+if (!MONGO_URI) {
+  throw new Error('You must provide a MongoDB URI');
+}
 
+  // connect to Mongo when the app initializes
+  const options = {
+    promiseLibrary: global.Promise,
+    useMongoClient: true
+  }
+
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_URI, options).then(db => console.log('connected to MongoDB'))
+
+app.use(bodyParser.json());
 app.use('/graphql', expressGraphQL({
   schema,
   graphiql: true
-}))
+}));
 
-app.listen(4000, () => {
-  console.log('Listening..')
-})
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('../webpack.config.js');
+app.use(webpackMiddleware(webpack(webpackConfig)));
+
+module.exports = app;
