d698e0e781a89da40a620f7acfec64db68394499
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2018-11-03 16:05:09 +0530

    ADD(method): renderSongs()

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -3,23 +3,32 @@ import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
 
 class SongList extends Component {
-    render() {
-        return (
-        <div>
-            SongList
-        </div>
-        )
-    }
+  renderSongs () {
+    return this.props.data.songs.map(song => {
+      return (
+        <li key={song.id} className='collection-item'>
+          { song.title }
+        </li>
+      )
+    })
+  }
+  render () {
+    if (this.props.data.loading) { return <div>Loading...</div> }
+    return (
+      <div className='collection'>
+        { this.renderSongs() }
+      </div>
+    )
+  }
 }
 
 const query = gql`
     {
         songs {
+            id
             title
         }
     }
-
-}
 `
 
-export default graphql (query)(SongList)
+export default graphql(query)(SongList)
