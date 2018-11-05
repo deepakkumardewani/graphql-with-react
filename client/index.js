a9d189d92e489df6fd822c678eeac5805ae026fa
Deepak Dewani <deepak.dewani@accionlabs.com>
2018-11-05 18:03:50 +0530

    ADD(route): to get song detail

--- client/index.js
+++ client/index.js
@@ -7,10 +7,10 @@ import { ApolloProvider } from 'react-apollo'
 import App from './components/App'
 import SongList from './components/SongList'
 import SongCreate from './components/SongCreate'
+import SongDetail from './components/SongDetail'
+
 
 const client = new ApolloClient({})
-
-
 
 const Root = () => {
   return (
@@ -18,7 +18,8 @@ const Root = () => {
       <Router history={hashHistory}>
         <Route path="/" component={App}>
           <IndexRoute component={SongList} />
-          <Route path="/songs/new" component={SongCreate}></Route>
+          <Route path="songs/new" component={SongCreate} />
+          <Route path="songs/:id" component={SongDetail} />
         </Route>
       </Router>
     </ApolloProvider>
